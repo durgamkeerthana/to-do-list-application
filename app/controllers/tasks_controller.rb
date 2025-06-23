3babe066768ae4239a2a92df8d6ec61a76f22ffd
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-06-23 14:18:04 +0530

    Configure the app for user specific tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,16 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_task, only: [:edit, :update, :destroy, :toggle]
 
   def index
-    @tasks = Task.order(created_at: :desc)
     @task = Task.new
-     @grouped_tasks = Task.order(created_at: :desc).group_by do |task|
-    date = task.created_at.to_date
-    if date == Date.today
-      "Today, #{date.strftime('%B %d, %Y')}"
-    elsif date == Date.yesterday
-      "Yesterday, #{date.strftime('%B %d, %Y')}"
-    elsif date.cweek == Date.today.cweek && date.year == Date.today.year
-      "This Week, #{date.strftime('%B %d, %Y')}"
-    elsif date.year == Date.today.year
-      date.strftime("%B %Y") # e.g., June 2025
-    else
-      date.strftime("%B %d, %Y") # e.g., May 14, 2024
+    @grouped_tasks = current_user.tasks.order(created_at: :desc).group_by do |task|
+      task.created_at.strftime("%B %Y")  
     end
   end
-end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       redirect_to root_path, notice: 'Task added successfully.'
     else
@@ -30,8 +19,7 @@ end
   end
 
   def edit
-  @task = Task.find(params[:id])
-end
+  end
 
   def update
     if @task.update(task_params)
@@ -54,7 +42,7 @@ end
   private
 
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def task_params
